64b791ac734eee32d335c283cd4b13210303cdef
Ellen Brøter <143997330+ellenbroter@users.noreply.github.com>
2024-02-14 14:07:10 +0100

    authentication services

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import firebaseConfig from "./firebaseConfig";
 import {initializeApp, onLog} from 'firebase/app';
 
 import {getFirestore, collection, addDoc, deleteDoc, doc, onSnapshot, updateDoc} from 'firebase/firestore';
+import {getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth';
 
 // INITIALIZE FIREBASE
 initializeApp(firebaseConfig);
@@ -56,7 +57,6 @@ deleteButton.addEventListener('click', (e)=>{
 onSnapshot(trainersCollection, (snapshot)=>{
 	const trainersArray = [];
 	snapshot.docs.forEach(doc => {
-		console.log(snapshot);
 		trainersArray.push({id: doc.id, ...doc.data()})
 	})
 	console.log(trainersArray);
@@ -80,3 +80,80 @@ updateButton.addEventListener('click', (e)=>{
 	})
 	.catch(err => console.log(err.message))
 })
+
+
+
+// PART 2 AUTTHENTICATION ----------------------------------------
+
+// INITIALIZING THE AUTHENTICATION SERVICE
+const authService = getAuth();
+
+// SIGNING UP USERS
+const email = document.querySelector('.email')
+const password = document.querySelector('.password')
+const signUpButton = document.querySelector('.sign-up-button')
+
+const signUpUsers = ()=>{
+	const userEmail = email.value;
+	const userPassword = password.value;
+	createUserWithEmailAndPassword(authService, userEmail, userPassword)
+	.then((cred)=> {
+		console.log('The account has been created successfully');
+		console.log(cred);
+	})
+	.catch(err => console.log(err.message))
+}
+
+signUpButton.addEventListener('click', (e)=>{
+	e.preventDefault();
+	signUpUsers();
+})
+
+
+// SIGN OUT USERS
+
+const signOutButton = document.querySelector('.sign-out-button');
+
+const signOutUsers = ()=>{
+	signOut(authService)
+	.then(()=> console.log('You have successfully signed out'))
+	.catch(err => console.log(err.message))
+}
+
+signOutButton.addEventListener('click', (e)=>{
+	e.preventDefault();
+	signOutUsers();
+})
+
+
+// LOGGING IN USERS
+const signInButton = document.querySelector('.log-in-button')
+const signInUsers = ()=>{
+	const userEmail = email.value;
+	const userPassword = password.value;
+	signInWithEmailAndPassword(authService, userEmail, userPassword)
+	.then(()=> {
+		console.log('You have successfully logged in')
+	})
+	.catch(err => console.log(err.message))
+}
+
+signInButton.addEventListener('click', (e)=>{
+	e.preventDefault();
+	signInUsers()
+})
+
+
+// CHECK USERS AUTHENTICATION STATE
+const checkUsersStatus = ()=>{
+	const secretContent = document.querySelector('.secret-content');
+	onAuthStateChanged(authService, user =>{
+		if(user){
+			secretContent.style.display = 'block';
+		} else{
+			secretContent.style.display = 'none';
+		}
+	})
+}
+
+checkUsersStatus();
